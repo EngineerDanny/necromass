20697d2cb14101a8dff4ef088168a84efc1e513c
EngineerDanny <agyapongdaniel7777@gmail.com>
2024-04-24 13:55:42 -0700

    Update mlr3resampling logic

--- bf.R
+++ bf.R
@@ -65,9 +65,20 @@ task.list <- list()
 for(out.i in 1:ncol(log.necro.dt)){
   task.dt <- data.table(log.necro.dt, Samples=meta.dt$Samples)
   task_id <- names(log.necro.dt)[[out.i]]
-  task.list[[task_id]] <- mlr3::TaskRegr$new(
+  reg.task <- mlr3::TaskRegr$new(
     task_id, task.dt, target=task_id
-  )$set_col_roles("Samples",c("group","stratum"))
+  )
+  
+  reg.task$col_roles$subset <- "Samples"
+  #reg.task$col_roles$group <- "Samples"
+  reg.task$col_roles$stratum <- "Samples"
+  reg.task$col_roles$feature <- setdiff(names(task.dt), c(task_id, "Samples"))
+  
+  #task.list[[task_id]] <- mlr3::TaskRegr$new(
+  #  task_id, task.dt, target=task_id
+  #)$set_col_roles("Samples",c("subset", "stratum"))
+  
+  task.list[[task_id]] <- reg.task
 }
 
 
@@ -100,6 +111,11 @@ LearnerRegrFuser <- R6Class("LearnerRegrFuser",
                                 X_train <- as.matrix(task$data(cols = task$feature_names))
                                 y_train <- as.matrix(task$data(cols = task$target_names))
                                 group_ind <- task$groups$group
+                                print("got here")
+                                print(group_ind)
+                                print("got here 2")
+                                print(task$strata)
+                                
                                 k <- as.numeric(length(unique(group_ind))) # number of groups
                                 n.group <- min(table(group_ind)) # minimum samples per group across all groups
                                 max_elements <- n.group * k  # Calculate the maximum number of elements to fit the matrix
@@ -186,161 +202,33 @@ LearnerRegrFuser <- R6Class("LearnerRegrFuser",
                             )
 )
 
-
-MyResampling = R6::R6Class("Resampling",
-  public = list(
-    id = NULL,
-    label = NULL,
-    param_set = NULL,
-    instance = NULL,
-    task_hash = NA_character_,
-    task_nrow = NA_integer_,
-    duplicated_ids = NULL,
-    man = NULL,
-    initialize = function(id, param_set = ps(), duplicated_ids = FALSE, label = NA_character_, man = NA_character_) {
-      self$id = checkmate::assert_string(id, min.chars = 1L)
-      self$label = checkmate::assert_string(label, na.ok = TRUE)
-      self$param_set = paradox::assert_param_set(param_set)
-      self$duplicated_ids = checkmate::assert_flag(duplicated_ids)
-      self$man = checkmate::assert_string(man, na.ok = TRUE)
-    },
-    format = function(...) {
-      sprintf("<%s>", class(self)[1L])
-    },
-    print = function(...) {
-      cat(format(self), if (is.null(self$label) || is.na(self$label)) "" else paste0(": ", self$label))
-      cat("\n* Iterations:", self$iters)
-      cat("\n* Instantiated:", self$is_instantiated)
-      cat("\n* Parameters:\n")
-      str(self$param_set$values)
-    },
-    help = function() {
-      self$man
-    },
-    instantiate = function(task) {
-      task = mlr3::assert_task(mlr3::as_task(task))
-      folds = private$.combine(lapply(task$strata$row_id, private$.sample, task = task))
-      id.fold.groups <- folds[task$groups, on="row_id"]
-      uniq.fold.groups <- setkey(unique(id.fold.groups[, .(
-        test.fold=fold, test.group=group)]))
-      self$instance <- list(
-        iteration.dt=data.table(train.groups=c("all","same","other"))[
-        , data.table(uniq.fold.groups), by=train.groups][, iteration := .I],
-        id.dt=id.fold.groups)
-      for(iteration.i in 1:nrow(self$instance$iteration.dt)){
-        split.info <- self$instance$iteration.dt[iteration.i]
-        is.set.group <- list(
-          test=id.fold.groups[["group"]] == split.info[["test.group"]])
-        is.set.group[["train"]] <- switch(
-          split.info[["train.groups"]],
-          same=is.set.group[["test"]],
-          other=!is.set.group[["test"]],
-          all=rep(TRUE, nrow(id.fold.groups)))
-        is.set.fold <- list(
-          test=id.fold.groups[["fold"]] == split.info[["test.fold"]])
-        is.set.fold[["train"]] <- !is.set.fold[["test"]]
-        for(set.name in names(is.set.fold)){
-          is.group <- is.set.group[[set.name]]
-          is.fold <- is.set.fold[[set.name]]
-          set(
-            self$instance$iteration.dt,
-            i=iteration.i,
-            j=set.name,
-            value=list(id.fold.groups[is.group & is.fold, row_id]))
-        }
-      }
-      self$task_hash = task$hash
-      self$task_nrow = task$nrow
-      invisible(self)
-    },
-    train_set = function(i) {
-      self$instance$iteration.dt$train[[i]]
-    },
-    test_set = function(i) {
-      self$instance$iteration.dt$test[[i]]
-    }
-  ),
-  active = list(
-    is_instantiated = function(rhs) {
-      !is.null(self$instance)
-    },
-    hash = function(rhs) {
-      if (!self$is_instantiated) {
-        return(NA_character_)
-      }
-      mlr3misc::calculate_hash(list(class(self), self$id, self$param_set$values, self$instance))
-    }
-  )
-)
-MyResamplingCV = R6::R6Class("MyResamplingCV", inherit = MyResampling,
-  public = list(
-    initialize = function() {
-      ps = paradox::ps(
-        folds = paradox::p_int(2L, tags = "required")
-      )
-      ps$values = list(folds = 10L)
-      super$initialize(
-        id = "mycv",
-        param_set = ps,
-        label = "Cross-Validation",
-        man = "TODO")
-    }
-  ),
-  active = list(
-    iters = function(rhs) {
-      nrow(mycv$instance$iteration.dt)
-    }
-  ),
-  private = list(
-    .sample = function(ids, ...) {
-      data.table(
-        row_id = ids,
-        fold = sample(seq(0, length(ids)-1) %% as.integer(self$param_set$values$folds) + 1L),
-        key = "fold"
-      )
-    },
-    .combine = function(instances) {
-      rbindlist(instances, use.names = TRUE)
-    },
-    deep_clone = function(name, value) {
-      switch(name,
-        "instance" = copy(value),
-        "param_set" = value$clone(deep = TRUE),
-        value
-        )
-    }
-  )
-)
-
-mycv <- MyResamplingCV$new()
+mycv <- mlr3resampling::ResamplingSameOtherSizesCV$new()
 mycv$param_set$values$folds=2
 for(task in task.list){
   mycv$instantiate(task)
 }
+mycv$instance$iteration.dt
 
 (reg.learner.list <- list(
   mlr3learners::LearnerRegrCVGlmnet$new(),
   mlr3::LearnerRegrFeatureless$new(),
-  LearnerRegrFuser$new()
+ LearnerRegrFuser$new()
 ))
 
-#(reg.learner.list <- list(
-#  LearnerRegrFuser$new()
-#))
+(debug.grid <- mlr3::benchmark_grid(
+  task.list["bacteria.Bradyrhizobium"],
+  reg.learner.list,
+  mycv))
+future::plan("sequential")
+debug.result <- mlr3::benchmark(debug.grid)
+debug.result
 
 (reg.bench.grid <- mlr3::benchmark_grid(
   task.list,
   reg.learner.list,
   mycv))
 
-(debug.grid <- mlr3::benchmark_grid(
-  task.list["bacteria.Kaistia"],
-  reg.learner.list,
-  mycv))
-future::plan("sequential")
-debug.result <- mlr3::benchmark(debug.grid)
-
-reg.dir <- "data-2024-04-17-benchmark-reg"
+reg.dir <- "data-2024-04-23-benchmark-reg-2"
 reg <- batchtools::loadRegistry(reg.dir)
 unlink(reg.dir, recursive=TRUE)
 reg = batchtools::makeExperimentRegistry(
@@ -373,7 +261,7 @@ jobs.after[!is.na(error), .(error, task_id=sapply(prob.pars, "[[", "task_id"))][
 
 ids <- jobs.after[is.na(error), job.id]
 bmr = mlr3batchmark::reduceResultsBatchmark(ids, reg = reg)
-save(bmr, file="data-2024-04-17-benchmark.RData")
+save(bmr, file="data-2024-04-23-benchmark-2.RData")
 
 # search for predictions with NAs
 tab = as.data.table(bmr)
